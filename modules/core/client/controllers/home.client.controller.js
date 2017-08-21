45eede68d35ddb21cb3919662f0034e67ec6b55e
Rekha Thella <rekha@vitwit.com>
2017-08-21 13:08:21 +0530

    first commit

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,9 +4,22 @@
   angular
     .module('core')
     .controller('HomeController', HomeController);
-  HomeController.$inject = ['Authentication'];
-  function HomeController(Authentication) {
+  HomeController.$inject = ['Authentication', 'Notification'];
+  function HomeController(Authentication, Notification) {
     var vm = this;
     vm.authentication = Authentication;
+    vm.getInfo = getInfo;
+    function checkUrl(data) {
+      var expression = /((ftp|http|https?):\/\/)?(www\.)?[a-z0-9\-\.]{3,}\.[a-z]{3}$/;
+      console.log(expression.test(data));
+      // return expression.test(data);
+    }
+    function getInfo(url) {
+      if (checkUrl(url)) {
+
+      } else {
+        Notification.error({ message: 'invalid url', delay: 3000 });
+      }
+    }
   }
 }());
