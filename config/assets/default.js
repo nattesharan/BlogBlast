4b89adbb1653dc06cad27e1a896a55144d64a248
Rekha Thella <rekha@vitwit.com>
2017-08-26 15:13:07 +0530

    changes

--- config/assets/default.js
+++ config/assets/default.js
@@ -9,7 +9,9 @@ module.exports = {
         // bower:css
         'public/lib/bootstrap/dist/css/bootstrap.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.css'
+        'public/lib/angular-ui-notification/dist/angular-ui-notification.css',
+        'public/lib/angular-aria/angular-aria.css',
+        'public/lib/angular-material/angular-material.css'
         // endbower
       ],
       js: [
@@ -24,6 +26,8 @@ module.exports = {
         'public/lib/angular-ui-notification/dist/angular-ui-notification.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/angular-aria/angular-aria.js',
+        'public/lib/angular-material/angular-material.js'
         // endbower
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
